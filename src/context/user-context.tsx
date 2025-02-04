64e20bf82aa1718c06a99e009304da99c79e373d
hafidzfurqon <hafidzfurqonyet@gmail.com>
2025-02-04 12:56:21 +0700

    fix / user context value

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext } from "react";
+import { _users } from "src/_mock";
 
 export const userContext = createContext({})
 export const useAppContext = () => useContext(userContext);
@@ -7,16 +8,9 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
     const user = [{
       name: "hafidz ganteng",
     }];
-    const data = user;
-  
-    const userContextValue = 
-        {
-            users_data : data
-        }
-    
   
     return (
-      <userContext.Provider value={userContextValue}>
+      <userContext.Provider value={_users}>
         {children}
       </userContext.Provider>
     );
